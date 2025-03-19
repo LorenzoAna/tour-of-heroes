9798e5bcd91be381d1668e4dd39325b45c4e0602
Ana Lorenzo <ana.lorenzorivero@plexus.es>
2025-03-19 12:04:00 +0100

    step 4 - observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,24 @@
 //@Injectable: decorador que se usa para marcar una clase como un servicio que puede ser inyectado en otros componentes o servicios.
 import { Injectable } from '@angular/core';
+
+// rxjs: bibliteca para programacion asincrona
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-// rxjs: bibliteca para programacion asincrona
-import { Observable } from 'rxjs';
 
 @Injectable({
   // La opción providedIn: 'root' indica que el servicio está disponible en toda la aplicación.
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  // Inyecta el servicio para permitir que usemos sus métodos
+  constructor(private messageService: MessageService) {}
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,32 +1,37 @@
 // lista de heroes
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes', // etiqueta para el html
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
+  selectedHero?: Hero;
   heroes: Hero[] = [];
 
   //constructor: Inyecta el servicio HeroService en el componente. Esto permite que el componente use los métodos del servicio.
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   //Este método del ciclo de vida de Angular se llama automáticamente después de crear el componente.
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  //Llama al método getHeroes del servicio HeroService y asigna el resultado a la propiedad heroes del componente.
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
-
-  selectedHero?: Hero;
-
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  //Llama al método getHeroes del servicio HeroService, Se suscribe al observable, y asigna el resultado a la propiedad heroes del componente.
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }

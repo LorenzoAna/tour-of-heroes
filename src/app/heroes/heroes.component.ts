2a8402a12e7bfdd0f9aa8b25d9f1585d5e1302a5
Ana Lorenzo <ana.lorenzorivero@plexus.es>
2025-03-19 15:11:00 +0100

    step 5 - fix HeroDetail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes', // etiqueta para el html
@@ -11,23 +10,14 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
   heroes: Hero[] = [];
 
   //constructor: Inyecta el servicio HeroService en el componente. Esto permite que el componente use los métodos del servicio.
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   //Este método del ciclo de vida de Angular se llama automáticamente después de crear el componente.
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   //Llama al método getHeroes del servicio HeroService, Se suscribe al observable, y asigna el resultado a la propiedad heroes del componente.

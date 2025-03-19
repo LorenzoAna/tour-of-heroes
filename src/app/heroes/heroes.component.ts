88d0a8e058bb6c3d2ddebc6fe9b7bddee607fb1b
Ana Lorenzo Rivero <ana.lorenzorivero@plexus.tech>
2025-03-19 09:43:45 +0100

    step 3

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,10 @@
+// lista de heroes
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes',
+  selector: 'app-heroes', // etiqueta para el html
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
